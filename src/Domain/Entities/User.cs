0f88b5f1bf55f323ab6d24e1f950a7161efece09
Pilar Lopez <pilarlopez1705@gmail.com>
2024-07-22 16:49:13 -0300

    Correcciones finales

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -8,11 +8,11 @@ namespace Domain.Entities
         public int Id { get; set; }
 
         [Required]
-        public string? Username { get; set; }
-        public string? Name { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
 
     }
 }
